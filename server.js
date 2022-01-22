af0d3428e91de462446fbf40ff84fc967dd2675d
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-01-22 13:06:24 -0800

    added partnerRouter.js and added to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
