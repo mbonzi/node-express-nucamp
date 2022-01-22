9410ba02c3b3acb5b242c65ed1db9f56c2439f55
Madelyn Bonzi <madelyn.bonzi@gmail.com>
2022-01-22 10:29:01 -0800

    updated campsite ID router

--- server.js
+++ server.js
@@ -27,21 +27,3 @@ app.use((req, res) => {
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-/*   app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsites: ${req.params.campsiteId} to you`);
-});
-
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation was not supported on /campsites/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} with description ${req.body.description}`);
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});    */
